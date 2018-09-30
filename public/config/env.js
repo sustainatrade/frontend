cf032d4e15d03867fb6654e7c43201c2539419aa
jim <jim.faller@mangosoftapps.com>
2018-10-01 00:41:41 +0800

    remove env conf

--- public/config/env.js
+++
@@ -1,5 +0,0 @@
-localStorage.setItem('graphql', 'https://graph.sustainatrade.com/graphql');
-localStorage.setItem('token', 'dsfwwqewqqeq');
-localStorage.setItem('graphql-ws', 'wss://graphws.sustainatrade.com/subscriptions');
-localStorage.setItem('storage', 'https://storage.sustainatrade.com');
-localStorage.setItem('postPhotoPath', '/file/eco-trade-assets');
