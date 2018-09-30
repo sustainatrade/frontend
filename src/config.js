bb9d26507f3d86221302a802caabbe27dab6e58b
jim <jim.faller@mangosoftapps.com>
2018-10-01 02:53:55 +0800

    add config overrides

--- src/config.js
+++ src/config.js
@@ -1,20 +1,23 @@
-module.exports = {
-  graphqlServer: localStorage.getItem("graphql"),
-  graphqlWsServer: localStorage.getItem("graphql-ws"),
-  reqCredentials: "include",
-  sections: [
-    {
-      key: "buy",
-      color: "orange",
-      displayName: "Buying"
-    },
-    {
-      key: "sell",
-      color: "green",
-      displayName: "Selling"
+module.exports = Object.assign(
+  {
+    graphqlServer: localStorage.getItem("graphql"),
+    graphqlWsServer: localStorage.getItem("graphql-ws"),
+    reqCredentials: "include",
+    sections: [
+      {
+        key: "buy",
+        color: "orange",
+        displayName: "Buying"
+      },
+      {
+        key: "sell",
+        color: "green",
+        displayName: "Selling"
+      }
+    ],
+    posts: {
+      actionButtonSize: "tiny"
     }
-  ],
-  posts: {
-    actionButtonSize: "tiny"
-  }
-};
+  },
+  window.OVERRIDE_CONFIG
+);
