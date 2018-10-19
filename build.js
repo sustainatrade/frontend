25e966d2186a06c573ddd9355e2b9cd5b303415b
jim <jim.faller@mangosoftapps.com>
2018-10-19 22:56:07 +0800

    add precache to root html

--- build.js
+++ build.js
@@ -7,12 +7,34 @@ process.env["REACT_APP_RAND_HASH"] = nanoid();
 var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+
 const webpackConfigProd = require("react-scripts/config/webpack.config.prod");
 
 webpackConfigProd.plugins.push(
   new BundleAnalyzerPlugin({
     analyzerMode: "static",
     reportFilename: "report.html"
+  })
+);
+
+webpackConfigProd.plugins.push(
+  new SWPrecacheWebpackPlugin({
+    minify: true,
+    logger(message) {
+      if (message.indexOf("Total precache size is") === 0) {
+        // This message occurs for every build and is a bit too noisy.
+        return;
+      }
+      console.log(message);
+    },
+    staticFileGlobsIgnorePatterns: [/^[^\.]*(\.html)?$/],
+    runtimeCaching: [
+      {
+        urlPattern: /^[^\.]*(\.html)?$/,
+        handler: "networkFirst"
+      }
+    ]
   })
 );
 
