8ed2e28442494a3d8fc3be3d876b1ca8b478944b
jim <jim.faller@mangosoftapps.com>
2018-11-07 03:46:31 +0800

    context

--- src/components/icon-scroller/IconScroller.js
+++ src/components/icon-scroller/IconScroller.js
@@ -2,8 +2,11 @@ import React, { useContext } from "react";
 import "./IconScroller.css";
 import Button from "antd/lib/button";
 import Icon from "antd/lib/icon";
+import IconScrollerContext from "../../contexts/IconScrollerContext";
 
 export default function IconScroller({ height, width }) {
+  const {} = useContext(IconScrollerContext.Context);
+  
   return (
     <div className="icon-scoller" style={{ height, width }}>
       <Button shape="circle" size="large">
